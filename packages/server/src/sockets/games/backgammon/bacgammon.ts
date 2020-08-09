89ed3463284fbb19eb03c6f8548a8ff515515d59
Sarp ISIK <wdsarpisik@gmail.com>
2020-08-09 12:47:49 +0200

    Edit module imports

--- packages/server/src/sockets/games/backgammon/bacgammon.ts
+++ packages/server/src/sockets/games/backgammon/bacgammon.ts
@@ -1,26 +1,25 @@
 import io from 'socket.io';
-import { BACKGAMMON_TYPES } from 'types';
-import { rollDices } from './utils';
+import { EVENTS, Game, PLAYERS } from 'types/lib/backgammon';
 import { layout } from './constants';
-import { EVENTS } from 'types/lib/backgammon';
-import { round } from './round';
 import { handleBrokenPoint } from './handleBrokenPoint';
 import { handleUndoRound } from './handleUndoRound';
+import { round } from './round';
+import { rollDices } from './utils';
 
 export default function bacgammon(socket: io.Socket) {
     // Generate initial game
-    const initialGame: BACKGAMMON_TYPES.Game = {
+    const initialGame: Game = {
         id: '',
         white: 'white_id',
         black: 'black_id',
         rounds: [
             {
                 attempt: 0,
-                player: BACKGAMMON_TYPES.PLAYERS.WHITE,
+                player: PLAYERS.WHITE,
                 turn: 1,
                 brokens: {
-                    [BACKGAMMON_TYPES.PLAYERS.WHITE]: 0,
-                    [BACKGAMMON_TYPES.PLAYERS.BLACK]: 0,
+                    [PLAYERS.WHITE]: 0,
+                    [PLAYERS.BLACK]: 0,
                 },
                 dice: rollDices(),
                 id: Date.now(),
