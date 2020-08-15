5a6ecd067dd9f7a395777724fe55a4643db3a70c
Sarp ISIK <wdsarpisik@gmail.com>
2020-08-15 11:28:17 +0200

    Create build script

--- packages/server/scripts/dist.sh
+++ packages/server/scripts/dist.sh
@@ -1,7 +1,4 @@
-#!/bin/sh
 # If the directory, `dist`, doesn't exist, create `dist`
 stat dist || mkdir dist
 # Archive artifacts
-# zip dist/$npm_package_name.zip -r build package.json package-lock.json
-ls
-npm run build
+zip dist/$npm_package_name.zip -r dist package.json package-lock.json
