790dbb946e89081a5ca620c8c39f2ce7c776e8e0
Sarp ISIK <wdsarpisik@gmail.com>
2020-08-16 12:22:38 +0200

    SS create todos

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,6 +4,12 @@ import './LoadEnv'; // Must be the first import
 import app from '@server';
 import logger from '@shared/Logger';
 
+/*
+ * TODO:
+ * - [] remove finished game.
+ * - [] programmatically remove finished game.
+ */
+
 // Start the server
 const port = Number(process.env.PORT || 3000);
 app.listen(port, () => {
