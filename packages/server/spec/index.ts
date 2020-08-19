71395f928e32b41c74d05afdd032870c3a16f592
Sarp ISIK <wdsarpisik@gmail.com>
2020-08-19 12:08:25 +0200

    SS syntax edit

--- packages/server/spec/index.ts
+++ packages/server/spec/index.ts
@@ -35,9 +35,9 @@ jasmine.loadConfig({
 // On complete callback function
 jasmine.onComplete((passed: boolean) => {
     if (passed) {
-        logger.info('All tests have passed :)');
+        logger.info('All tests have passed.');
     } else {
-        logger.error('At least one test has failed :(');
+        logger.error('At least one test has failed.');
     }
 });
 
