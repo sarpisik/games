26af4f95f4c40c5bb52bf0cac458b773e65f0924
Sarp ISIK <wdsarpisik@gmail.com>
2020-08-19 22:49:41 +0200

    SS test games api PUT

--- packages/server/spec/backgammon/games.spec.ts
+++ packages/server/spec/backgammon/games.spec.ts
@@ -8,6 +8,8 @@ import { Game, PLAYERS } from '@shared-types/backgammon';
 describe('backgammon/games api', () => {
     const gamesPath = '/api/backgammon/games';
     const getGamePath = '/api/backgammon/games/:id';
+    const putGamePath = '/api/backgammon/games/:id';
+
     const games: Game[] = [
         {
             id: Date.now(),
@@ -43,6 +45,8 @@ describe('backgammon/games api', () => {
 
     let agent: SuperTest<Test>;
 
+    type Callback = supertest.CallbackHandler | undefined;
+
     // Use "done" callback to prevent error:
     // https://github.com/nodejs/node/issues/27261
     beforeAll((done) => {
@@ -51,12 +55,15 @@ describe('backgammon/games api', () => {
     });
 
     describe(`"GET:${gamesPath}"`, () => {
+        const callApi = (callback: Callback) =>
+            agent.get(gamesPath).end(callback);
+
         it(`should return a JSON object with all the games and a status code of "${OK}" if the request was successful.`, (done) => {
             spyOn(GamesService.prototype, 'readGames').and.returnValue(
                 Promise.resolve(games)
             );
 
-            agent.get(gamesPath).end((err, res) => {
+            callApi((err, res) => {
                 pErr(err);
                 expect(res.status).toBe(OK);
                 expect(res.body).toEqual(games);
@@ -69,7 +76,7 @@ describe('backgammon/games api', () => {
             const errMsg = 'Something went wrong.';
             spyOn(GamesService.prototype, 'readGames').and.throwError(errMsg);
 
-            agent.get(gamesPath).end((err, res) => {
+            callApi((err, res) => {
                 pErr(err);
                 expect(res.status).toBe(BAD_REQUEST);
                 expect(res.body.error).toBe(errMsg);
@@ -89,9 +96,8 @@ describe('backgammon/games api', () => {
             stages: 1,
         };
 
-        const callApi = (reqBody: ReqBody) => {
-            return agent.post(gamesPath).type('form').send(reqBody);
-        };
+        const callApi = (reqBody: ReqBody, callback: Callback) =>
+            agent.post(gamesPath).type('form').send(reqBody).end(callback);
 
         it(`should return a status code of "${CREATED}" if the request was successful.`, (done) => {
             const game = games[0];
@@ -100,7 +106,7 @@ describe('backgammon/games api', () => {
                 Promise.resolve(game)
             );
 
-            callApi(gameData).end((err, res) => {
+            callApi(gameData, (err, res) => {
                 pErr(err);
                 expect(res.status).toBe(CREATED);
                 expect(res.body).toEqual(game);
@@ -113,7 +119,7 @@ describe('backgammon/games api', () => {
             const errMsg = 'Something went wrong.';
             spyOn(GamesService.prototype, 'createGame').and.throwError(errMsg);
 
-            callApi(gameData).end((err, res) => {
+            callApi(gameData, (err, res) => {
                 pErr(err);
                 expect(res.status).toBe(BAD_REQUEST);
                 expect(res.body.error).toBe(errMsg);
@@ -124,7 +130,7 @@ describe('backgammon/games api', () => {
 
     describe(`"GET:${getGamePath}"`, () => {
         const url = getGamePath.replace(':id', '12345');
-        const callApi = () => agent.get(url);
+        const callApi = (callback: Callback) => agent.get(url).end(callback);
 
         it(`should return a JSON object with the game and a status code of "${OK}" if the request was successful.`, (done) => {
             const game = games[1];
@@ -133,7 +139,7 @@ describe('backgammon/games api', () => {
                 Promise.resolve(game)
             );
 
-            callApi().end((err, res) => {
+            callApi((err, res) => {
                 pErr(err);
                 expect(res.status).toBe(OK);
                 expect(res.body).toEqual(game);
@@ -146,7 +152,59 @@ describe('backgammon/games api', () => {
             const errMsg = 'Something went wrong.';
             spyOn(GamesService.prototype, 'readGame').and.throwError(errMsg);
 
-            callApi().end((err, res) => {
+            callApi((err, res) => {
+                pErr(err);
+                expect(res.status).toBe(BAD_REQUEST);
+                expect(res.body.error).toBe(errMsg);
+                done();
+            });
+        });
+    });
+
+    describe(`"PUT:${putGamePath}"`, () => {
+        type ReqBody = Pick<Game, 'players'>;
+
+        let game = games[2];
+        const url = getGamePath.replace(':id', game.id.toString());
+
+        const gameData: ReqBody = {
+            players: {
+                [PLAYERS.BLACK]: Date.now(),
+                [PLAYERS.WHITE]: Date.now(),
+            },
+        };
+
+        const callApi = (reqBody: ReqBody, callback: Callback) =>
+            agent.put(url).type('form').send(reqBody).end(callback);
+
+        it(`should return a status code of "${OK}" if the request was successful.`, (done) => {
+            spyOn(GamesService.prototype, 'readGame').and.returnValue(
+                Promise.resolve(game)
+            );
+
+            game = Object.assign({}, game, gameData);
+
+            spyOn(GamesService.prototype, 'updateGame').and.returnValue(
+                Promise.resolve(game)
+            );
+
+            callApi(gameData, (err, res) => {
+                pErr(err);
+                expect(res.status).toBe(OK);
+                expect(res.body).toEqual(game);
+                expect(res.body.error).toBeUndefined();
+                done();
+            });
+        });
+
+        it(`should return a JSON object containing an error message and a status code of "${BAD_REQUEST}" if the request was unsuccessful.`, (done) => {
+            const errMsg = 'Something went wrong.';
+            spyOn(GamesService.prototype, 'readGame').and.returnValue(
+                Promise.resolve(game)
+            );
+            spyOn(GamesService.prototype, 'updateGame').and.throwError(errMsg);
+
+            callApi(gameData, (err, res) => {
                 pErr(err);
                 expect(res.status).toBe(BAD_REQUEST);
                 expect(res.body.error).toBe(errMsg);
