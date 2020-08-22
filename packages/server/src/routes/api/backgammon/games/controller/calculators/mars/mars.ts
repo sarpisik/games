3cbe21f42e4d2d758b9c2fa063647434fb3ef114
Sarp ISIK <wdsarpisik@gmail.com>
2020-08-22 21:29:02 +0200

    test

--- packages/server/src/routes/api/backgammon/games/controller/calculators/mars/mars.ts
+++ packages/server/src/routes/api/backgammon/games/controller/calculators/mars/mars.ts
@@ -1,10 +1,13 @@
 import { Round, OPPONENT } from '@shared-types/backgammon';
+import { customPromise } from '@shared/customPromise';
 
 export default async function calculateMars(
     winner: Round['player'],
     collected: Round['collected']
 ) {
-    const loserCollected = collected[OPPONENT[winner]];
+    const loserCollected = await customPromise(
+        () => collected[OPPONENT[winner]]
+    );
 
     return loserCollected === 0;
 }
