7ab620a8319ef01aa1f9acc0a3d8fcc029b55192
Sarp ISIK <wdsarpisik@gmail.com>
2020-08-23 13:19:36 +0200

    CS recalculate width on landscape

--- packages/backgammon/src/app/slices/measures/utils/sizes/sizes.ts
+++ packages/backgammon/src/app/slices/measures/utils/sizes/sizes.ts
@@ -9,7 +9,7 @@ export default function calculateSizes() {
         windowWidth,
     } = calculateWindowDimension();
 
-    const BOARD_WIDTH = windowWidth;
+    const BOARD_WIDTH = isLandscape ? windowWidth * 0.75 : windowWidth;
     const BOARD_HEIGHT = isLandscape ? windowHeight : windowWidth;
 
     return {
