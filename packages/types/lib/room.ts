1b1dc307285139b3c1102aa39275e029213ec95e
Sarp ISIK <wdsarpisik@gmail.com>
2020-08-28 16:23:14 +0200

    SS define EmitEditGame

--- packages/types/lib/room.ts
+++ packages/types/lib/room.ts
@@ -1,3 +1,5 @@
+import { GameClient } from "./backgammon";
+
 export enum ROOM_EVENTS {
     NEW_USER = "NEW_USER",
     JOIN_ROOM = "JOIN_ROOM",
@@ -6,3 +8,7 @@ export enum ROOM_EVENTS {
 
 export type EmitJoinRooms = EmitJoinRoom[];
 export type EmitJoinRoom = number;
+export type EmitEditGame = Pick<
+    GameClient,
+    "id" | "duration" | "players" | "stages"
+>;
