a43d1bfa6e030f317caaa66de9b273f54e62c44c
Sarp ISIK <wdsarpisik@gmail.com>
2020-08-28 12:56:39 +0200

    CS style Game

--- packages/backgammon/src/views/room/components/Games/components/Game/Game.tsx
+++ packages/backgammon/src/views/room/components/Games/components/Game/Game.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import { FaUser, FaRegUser } from 'react-icons/fa';
+import { AiTwotoneSetting } from 'react-icons/ai';
 
 interface GameProps {
     url: string;
@@ -12,7 +15,17 @@ export default function Game(props: GameProps): React.ReactElement {
 
     return (
         <Col key={url}>
-            <Link to={url}>{children}</Link>
+            <Card>
+                <Card.Header className="d-flex align-items-center justify-content-between">
+                    <Link to={url}>{children}</Link>
+                    <AiTwotoneSetting />
+                </Card.Header>
+                <Card.Body className="d-flex align-items-center justify-content-between">
+                    <FaUser />
+                    -
+                    <FaRegUser />
+                </Card.Body>
+            </Card>
         </Col>
     );
 }
