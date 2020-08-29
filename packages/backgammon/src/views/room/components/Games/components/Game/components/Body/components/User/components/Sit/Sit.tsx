caea133f1342d42aaa3c13029e51cf3c4191a199
Sarp ISIK <wdsarpisik@gmail.com>
2020-08-29 12:21:33 +0200

    CS syntax refactor

--- packages/backgammon/src/views/room/components/Games/components/Game/components/Body/components/User/components/Sit/Sit.tsx
+++ packages/backgammon/src/views/room/components/Games/components/Game/components/Body/components/User/components/Sit/Sit.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-import { GameClient, PLAYERS } from 'types/lib/backgammon';
 import usePlayer, { UsePlayerParams } from './hooks/usePlayer/usePlayer';
-
-interface SitProps {
-    gameId: GameClient['id'];
-    color: keyof typeof PLAYERS;
-}
 
 export default function Sit(props: UsePlayerParams): React.ReactElement {
     const setPlayer = usePlayer(props);
