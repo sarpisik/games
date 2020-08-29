12e06802309e805cc15eb7b7a8e15492c9c59543
Sarp ISIK <wdsarpisik@gmail.com>
2020-08-29 17:01:14 +0200

    CS create hoc withAuthentication

---
+++ packages/backgammon/src/components/WithAuthentication/WithAuthentication.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import awsconfig from '../../aws-exports';
+
+Amplify.configure(awsconfig);
+
+export default function withAuthentication<Props>(
+    WrappedComponent: React.ComponentType<Props>
+) {
+    return function WithAuthentication(props: Props): React.ReactElement {
+        const [user, setUser] = React.useState<any>(null);
+
+        React.useEffect(() => {
+            Hub.listen('auth', ({ payload: { event, data } }) => {
+                switch (event) {
+                    case 'signIn':
+                    case 'cognitoHostedUI':
+                        getUser().then((userData) => setUser(userData));
+                        break;
+                    case 'signOut':
+                        setUser(null);
+                        break;
+                    case 'signIn_failure':
+                    case 'cognitoHostedUI_failure':
+                        console.log('Sign in failure', data);
+                        break;
+                }
+            });
+
+            getUser().then((userData) => setUser(userData));
+        }, []);
+
+        function getUser() {
+            return Auth.currentAuthenticatedUser()
+                .then((userData) => userData)
+                .catch(() => console.log('Not signed in'));
+        }
+        return (
+            <React.Fragment>
+                <p>User: {user ? JSON.stringify(user.attributes) : 'None'}</p>
+                {user ? (
+                    <button onClick={() => Auth.signOut()}>Sign Out</button>
+                ) : (
+                    <button onClick={() => Auth.federatedSignIn()}>
+                        Federated Sign In
+                    </button>
+                )}
+                <WrappedComponent {...props} />
+            </React.Fragment>
+        );
+    };
+}
