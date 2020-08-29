ca0466d3152ca9f8daca036cc55ab5e0b935bd02
Sarp ISIK <wdsarpisik@gmail.com>
2020-08-29 12:21:12 +0200

    CS create hook

--- packages/backgammon/src/views/room/components/Games/components/Game/components/Body/components/User/components/Sit/Sit.tsx
+++ packages/backgammon/src/views/room/components/Games/components/Game/components/Body/components/User/components/Sit/Sit.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-import { PLAYERS, GameClient } from 'types/lib/backgammon';
-import { useEditGame } from '../../../../../../../shared/hooks';
-import { useUser } from '../../../../../../../../../../../../app/slices';
+import { GameClient, PLAYERS } from 'types/lib/backgammon';
+import usePlayer, { UsePlayerParams } from './hooks/usePlayer/usePlayer';
 
 interface SitProps {
     gameId: GameClient['id'];
     color: keyof typeof PLAYERS;
 }
 
-export default function Sit(props: SitProps): React.ReactElement {
-    const { gameId, color } = props;
-    const { user } = useUser();
-    const { game, editGame } = useEditGame(gameId);
-    const setPlayer = () => {
-        const players = Object.assign({}, game.players, {
-            [PLAYERS[color]]: user.id,
-        });
-        const payload = Object.assign({}, game, { players });
-
-        editGame(payload);
-    };
+export default function Sit(props: UsePlayerParams): React.ReactElement {
+    const setPlayer = usePlayer(props);
 
     return (
         <Button onClick={setPlayer} block>
