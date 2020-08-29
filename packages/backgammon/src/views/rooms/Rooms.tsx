d4b3bf8cd75457cf0efaeaace91c6c14dec9c3f0
Sarp ISIK <wdsarpisik@gmail.com>
2020-08-29 17:04:39 +0200

    CS authenticate rooms view

--- packages/backgammon/src/views/rooms/Rooms.tsx
+++ packages/backgammon/src/views/rooms/Rooms.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useRooms } from '../../app/slices';
 import { withRoomsConnection } from './components';
+import { withAuthentication } from '../../components';
 
-export default withRoomsConnection(Rooms);
+export default withAuthentication(withRoomsConnection(Rooms));
 
 function Rooms(): React.ReactElement {
     const rooms = useRooms();
