2b86ce74d01a048a89ec963c6e4ef6bd52850931
Sarp ISIK <wdsarpisik@gmail.com>
2020-08-30 13:17:31 +0200

    CS create graphql UsersAPI

---
+++ packages/backgammon/src/graphql/subscriptions.ts
@@ -0,0 +1,46 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser {
+    onCreateUser {
+      id
+      name
+      email
+      wins
+      loses
+      escapes
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser {
+    onUpdateUser {
+      id
+      name
+      email
+      wins
+      loses
+      escapes
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser {
+    onDeleteUser {
+      id
+      name
+      email
+      wins
+      loses
+      escapes
+      createdAt
+      updatedAt
+    }
+  }
+`;
