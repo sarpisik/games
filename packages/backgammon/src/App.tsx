8c7f0ec61f5487c3c8ab3692a09353e5ed3d4da5
Sarp ISIK <wdsarpisik@gmail.com>
2020-09-01 15:50:05 +0200

    CS trigger rebuild

--- packages/backgammon/src/App.tsx
+++ packages/backgammon/src/App.tsx
@@ -7,6 +7,7 @@ import { ROUTES } from './config';
 /*
  * TODO:
  * - [] display only round player related errors.
+ * - [] display only round player related errors.
  * - [] add surrender button.
  */
 
