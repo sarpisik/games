db705747d66e5d4c8a6186e90623f503e9faf596
Sarp ISIK <wdsarpisik@gmail.com>
2020-09-02 13:56:18 +0200

    SS test handleGameOver

---
+++ packages/server/src/routes/api/backgammon/rooms/room/game/methods/handleGameOver/__test__/handleGameOver.spec.ts
@@ -0,0 +1,23 @@
+import { EmitStageOver, PLAYERS } from '@shared-types/backgammon';
+import { GAME_EVENTS } from '@shared-types/game';
+import handleGameOver from '../handleGameOver';
+
+describe('handleGameOver', () => {
+    const STATUS = 'OVER';
+
+    it(`should set status to "${STATUS}" and emits "${GAME_EVENTS.GAME_OVER}" event`, () => {
+        const _emitNamespace = jasmine.createSpy();
+        const backgammonGame = { _status: '', _emitNamespace };
+        const payload: EmitStageOver = { winner: PLAYERS.BLACK };
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        handleGameOver.call(backgammonGame, payload);
+
+        expect(_emitNamespace).toHaveBeenCalledTimes(1);
+        expect(_emitNamespace).toHaveBeenCalledWith(
+            GAME_EVENTS.GAME_OVER,
+            payload
+        );
+    });
+});
