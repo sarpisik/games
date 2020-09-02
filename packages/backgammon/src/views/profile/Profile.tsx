f0875d1ac0718b90882583bebf9f2cfd6ff65c7a
Sarp ISIK <wdsarpisik@gmail.com>
2020-09-02 20:56:19 +0200

    CS create basic profile page

--- packages/backgammon/src/views/profile/Profile.tsx
+++ packages/backgammon/src/views/profile/Profile.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
 import { RouteComponentProps } from 'react-router-dom';
+import { useUser } from '../../app/slices';
 import { withAuthorization } from '../../components';
 
 /*
@@ -12,5 +20,33 @@ import { withAuthorization } from '../../components';
 export default withAuthorization(Profile);
 
 function Profile(_props: RouteComponentProps): React.ReactElement {
-    return <div>profile</div>;
+    const { user } = useUser();
+
+    return (
+        <Container>
+            <Row className="justify-content-md-center">
+                <Col md={8}>
+                    <Card>
+                        <Card.Header>Profile</Card.Header>
+                        <Card.Body>
+                            <Card.Title>{user.name}</Card.Title>
+                            <Card.Text>
+                                User description will be here.
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroupItem>{user.email}</ListGroupItem>
+                            <ListGroupItem>{user.createdAt}</ListGroupItem>
+                            <ListGroupItem>{user.wins}</ListGroupItem>
+                            <ListGroupItem>{user.loses}</ListGroupItem>
+                            <ListGroupItem>{user.escapes}</ListGroupItem>
+                            <ListGroupItem>
+                                <Button variant="danger">Delete</Button>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
