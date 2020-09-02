d21aba1e7f677ed1fe082fd5331f8c4467250e14
Sarp ISIK <wdsarpisik@gmail.com>
2020-09-02 10:10:56 +0200

    SS test initializeGame

---
+++ packages/server/src/routes/api/backgammon/rooms/room/game/methods/initializeGame/__test__/initializeGame.spec.ts
@@ -0,0 +1,26 @@
+import { PLAYERS } from '@shared-types/backgammon';
+import initializeGame from '../initializeGame';
+
+describe('initializeGame', () => {
+    it(`should call round initializer method with passed "${
+        PLAYERS[PLAYERS.WHITE]
+    }" player after setting the game status and the rounds`, () => {
+        const backgammonGame = {
+            rounds: [],
+            _status: '',
+            _initializeRound: jasmine.createSpy(),
+        };
+        const roundPlayer = PLAYERS.WHITE;
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        initializeGame.call(backgammonGame, roundPlayer);
+
+        expect(backgammonGame.rounds).toEqual([]);
+        expect(backgammonGame._status).toBe('INITIALIZED');
+        expect(backgammonGame._initializeRound).toHaveBeenCalledWith(
+            roundPlayer
+        );
+        expect(backgammonGame._initializeRound).toHaveBeenCalledTimes(1);
+    });
+});
