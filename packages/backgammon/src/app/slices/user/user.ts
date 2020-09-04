ed974c40721d72edb07e3c7d7696a246bf73fc51
Sarp ISIK <wdsarpisik@gmail.com>
2020-09-04 11:28:28 +0200

    CS define reducer setUserState

--- packages/backgammon/src/app/slices/user/user.ts
+++ packages/backgammon/src/app/slices/user/user.ts
@@ -1,5 +1,9 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { User } from '../../../types/user';
+import { User as U } from '../../../types/user';
+
+interface User extends U {
+    state: 'INITIAL' | 'LOADING' | 'SUCCESS' | 'ERROR';
+}
 
 export const initialState: User = {
     id: '',
@@ -11,18 +15,22 @@ export const initialState: User = {
     escapes: 0,
     createdAt: '',
     updatedAt: '',
+    state: 'INITIAL',
 };
 
 export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        setUser(state, action: PayloadAction<User>) {
+        setUserState(state, action: PayloadAction<Pick<User, 'state'>>) {
+            Object.assign(state, action.payload);
+        },
+        setUser(state, action: PayloadAction<U>) {
             Object.assign(state, action.payload);
         },
     },
 });
 
-export const { setUser } = userSlice.actions;
+export const { setUser, setUserState } = userSlice.actions;
 
 export default userSlice.reducer;
