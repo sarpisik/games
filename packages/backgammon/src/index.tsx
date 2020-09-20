cae0b346c5d2d62b05d3f44d4dafc64ea8f0ac9c
Sarp ISIK <wdsarpisik@gmail.com>
2020-09-20 18:44:14 +0200

    CS add localize

--- packages/backgammon/src/index.tsx
+++ packages/backgammon/src/index.tsx
@@ -7,6 +7,8 @@ import { Router } from 'react-router';
 import { history } from './lib';
 
 import App from './App';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
@@ -15,11 +17,13 @@ import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <Router history={history}>
-                <App />
-            </Router>
-        </Provider>
+        <I18nextProvider i18n={i18n}>
+            <Provider store={store}>
+                <Router history={history}>
+                    <App />
+                </Router>
+            </Provider>
+        </I18nextProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
--- packages/backgammon/src/views/home/Home.tsx
+++ packages/backgammon/src/views/home/Home.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { User } from './components';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../../configs';
+import { User } from './components';
 
 export default function Home(): React.ReactElement {
+    const { t } = useTranslation();
+
     return (
-        <ul className="p-0 m-0">
-            <Link className="mr-3" to={ROUTES.ROOMS}>
-                Rooms
-            </Link>
-            <Link className="mr-3" to={ROUTES.PROFILE}>
-                Profile
-            </Link>
+        <ul className="p-0 m-0 text-capitalize">
+            <li>
+                <Link className="mr-3" to={ROUTES.ROOMS}>
+                    {t('rooms')}
+                </Link>
+            </li>
+            <li>
+                <Link className="mr-3" to={ROUTES.PROFILE}>
+                    {t('profile')}
+                </Link>
+            </li>
             <User />
         </ul>
     );
