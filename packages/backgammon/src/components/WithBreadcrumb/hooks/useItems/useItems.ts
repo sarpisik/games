788dff62b4e5986a90eb69453b04455ca39eba92
Sarp ISIK <wdsarpisik@gmail.com>
2020-09-22 18:54:02 +0200

    CS fix breadcrumbs

--- packages/backgammon/src/components/WithBreadcrumb/hooks/useItems/useItems.ts
+++ packages/backgammon/src/components/WithBreadcrumb/hooks/useItems/useItems.ts
@@ -3,12 +3,17 @@ import { useTranslation } from 'react-i18next';
 
 export default function useItems(url: string) {
     const { t } = useTranslation();
+
     const paths = url.split('/');
+    // Delete path to locale page
+    paths.shift();
+    // Delete the current page.
     paths.pop();
 
     const items = paths.map((p, i) => {
         const shouldHome = i === 0;
-        const to = shouldHome ? '/' : paths.slice(0, i + 1).join('/');
+        const to = '/' + paths.slice(0, i + 1).join('/');
+
         return {
             key: to,
             active: to === url,
