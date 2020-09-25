aec0b53709664489155997a7fb8b8b2a786c5609
Sarp ISIK <wdsarpisik@gmail.com>
2020-09-25 13:07:43 +0200

    CS fix landscape mode layout

--- packages/backgammon/src/utils/calculateWindowDimension/calculateWindowDimension.ts
+++ packages/backgammon/src/utils/calculateWindowDimension/calculateWindowDimension.ts
@@ -3,7 +3,7 @@ export default function calculateWindowDimension() {
     const windowHeight = window.innerHeight;
     // Use availheight to avoid keyboard height in mobile devices
     const orientation = windowWidth / window.screen.availHeight;
-    const isLandscape = orientation > 1 && windowWidth >= 1280;
+    const isLandscape = orientation > 1;
 
     return { isLandscape, windowHeight, windowWidth, orientation };
 }
