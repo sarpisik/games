c1f3d1bd95d900d6e35fc1da07abd462dca65462
Sarp ISIK <wdsarpisik@gmail.com>
2020-10-01 10:39:35 +0200

    CS fix issue safari layout home page

--- packages/backgammon/src/views/home/Home.tsx
+++ packages/backgammon/src/views/home/Home.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
-import Row from 'react-bootstrap/Row';
 import { Trans } from 'react-i18next';
 import { Link, RouteComponentProps } from 'react-router-dom';
 import { withLayout, withLocaleSwitch } from '../../components';
 import { ROUTES } from '../../configs';
 import { User } from './components';
 
-export default withLocaleSwitch(withLayout()(Home));
+export default withLocaleSwitch(
+    withLayout({ className: 'd-flex justify-content-sm-center' })(Home)
+);
 
 function Home(props: RouteComponentProps): React.ReactElement {
     const {
@@ -16,25 +17,23 @@ function Home(props: RouteComponentProps): React.ReactElement {
     } = props;
 
     return (
-        <Row className="h-100 justify-content-sm-center">
-            <Col className="d-flex flex-column justify-content-center" sm={6}>
-                <ListGroup className="text-capitalize text-center">
-                    <ListGroup.Item>
-                        <Link className="d-block" to={`${url}${ROUTES.ROOMS}`}>
-                            <Trans i18nKey="links.rooms" />
-                        </Link>
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                        <Link
-                            className="d-block"
-                            to={`${url}${ROUTES.PROFILE}`}
-                        >
-                            <Trans i18nKey="links.profile" />
-                        </Link>
-                    </ListGroup.Item>
-                    <User />
-                </ListGroup>
-            </Col>
-        </Row>
+        <Col
+            className="flex-grow-1 d-flex flex-column justify-content-center"
+            sm={6}
+        >
+            <ListGroup className="text-capitalize text-center">
+                <ListGroup.Item>
+                    <Link className="d-block" to={`${url}${ROUTES.ROOMS}`}>
+                        <Trans i18nKey="links.rooms" />
+                    </Link>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <Link className="d-block" to={`${url}${ROUTES.PROFILE}`}>
+                        <Trans i18nKey="links.profile" />
+                    </Link>
+                </ListGroup.Item>
+                <User />
+            </ListGroup>
+        </Col>
     );
 }
